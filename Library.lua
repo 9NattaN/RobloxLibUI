774086d428f6402431ca5c27df3e01cfed291059
9NattaN <100978159+9NattaN@users.noreply.github.com>
2023-08-16 20:59:28 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -29,12 +29,7 @@ local Library = {
 
     HudRegistry = {};
 
-    FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
-    OutlineColor = Color3.fromRGB(50, 50, 50);
-    RiskColor = Color3.fromRGB(255, 50, 50),
+
 
     Black = Color3.new(0, 0, 0);
     Font = Enum.Font.Code,
@@ -46,8 +41,8 @@ local Library = {
     ScreenGui = ScreenGui;
 };
 
-local RainbowStep = 0
-local Hue = 0
+local RainbowStep = 1
+local Hue = 1
 
 table.insert(Library.Signals, RenderStepped:Connect(function(Delta)
     RainbowStep = RainbowStep + Delta
