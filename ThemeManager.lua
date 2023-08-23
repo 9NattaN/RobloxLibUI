37639b36f2b8ec214b8b3a0626565918f380027d
9NattaN <100978159+9NattaN@users.noreply.github.com>
2023-08-23 17:07:58 +0700

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -1,6 +1,6 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'TBLOX HUB(Ro Ghoul)'
+	ThemeManager.Folder = 'TBLOX HUB (Ro Ghoul)'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
