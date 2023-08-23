b22d93f7d6e38204408a90321d42244da91338e2
9NattaN <100978159+9NattaN@users.noreply.github.com>
2023-08-23 16:58:51 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'TBLOX HUB'
+	SaveManager.Folder = 'LinoriaLibSettings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
